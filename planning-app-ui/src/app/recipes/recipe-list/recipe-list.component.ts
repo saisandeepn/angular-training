c77508553e218a2af823c54c913915e2fd2ad944
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2021-05-23 22:31:16 -0400

    added components for recipe list

--- planning-app-ui/src/app/recipes/recipe-list/recipe-list.component.ts
+++ planning-app-ui/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe","A test Recipe","https://th.bing.com/th/id/OIP.D7CM2Z6_ITS7nZNSq3vwigHaHa?pid=ImgDet&rs=1")
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
