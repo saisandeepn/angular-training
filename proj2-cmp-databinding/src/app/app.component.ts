3d0553d56e6be53eb564353c7a33b550ca50c983
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2021-05-26 23:31:25 -0400

    binding to custom events using Output()

--- proj2-cmp-databinding/src/app/app.component.ts
+++ proj2-cmp-databinding/src/app/app.component.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [
-    { type: 'server', name: 'Test server', content: 'Test Content' }
+    { type: 'server', name: 'Test server', content: 'Test Content' },
   ];
+
+  constructor(){}
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
 }
