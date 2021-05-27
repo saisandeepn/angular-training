f6b7b238c408e06c2617f20d75a8689c79d667a5
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2021-05-27 00:43:47 -0400

    added local reference example

--- proj2-cmp-databinding/src/app/cockpit/cockpit.component.ts
+++ proj2-cmp-databinding/src/app/cockpit/cockpit.component.ts
@@ -14,7 +14,8 @@ export class CockpitComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value)
     this.serverCreated.emit({
       serverName: this.newServerName,
       serverContent: this.newServerContent,
